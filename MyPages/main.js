3a1603e22df119962f571ae2fa4a462818b81be1
Carsten Ruhl <ToxicGecko13@Gmail.com>
2018-12-07 15:07:56 -0800

    List Name and List Content

--- MyPages/main.js
+++ MyPages/main.js
@@ -1,10 +1,10 @@
 // main.js: This code starts to run after the HTML page is loaded, because of defer attribute in script tag.  
-let boardGames = ['Catan', 'Pandemic', 'Ticket to Ride', 'Harbour', 'King of Tokyo', 'Forbidden Island', 'Dragonwood', 'Lost Cities'];  
+let boardGames = ['Bored: Break The Safe', 'Bored: The Chameleon', 'Bored: Clue', 'Bored: Monopoly', 'Video: Brawlhalla', 'Video: Skyrim', 'Video: Overwatch', 'Card: Boy Scout'];  
 
 boardGames.reverse();
 
 // Update the title with the total number of games from our array length  
-document.getElementById('ListTitle').textContent = "Top " + boardGames.length + " Board Games"  
+document.getElementById('ListTitle').textContent = "Carsten's Top " + boardGames.length + " Games"  
 
 // Retrieve the ordered list element (ol) so we can add list items (li)
 let listParent = document.querySelector('.TopTen');  
